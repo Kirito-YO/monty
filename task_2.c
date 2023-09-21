ecc5215e69fd2b53f2a7345f0638c2056786c691
Kirito-YO <ussf.lord@gmail>
2023-09-21 15:34:28 -0700

    fix betty

--- task_2.c
+++ task_2.c
@@ -7,17 +7,17 @@
  */
 void rm_stack(stack_t **stk_tete, unsigned int useless_ctr)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    if (*stk_tete == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", useless_ctr);
-        fclose(monty_bus.monty_file);
-        free(monty_bus.line_content);
-        free_stack(*stk_tete);
-        exit(EXIT_FAILURE);
-    }
-    temp = *stk_tete;
-    *stk_tete = temp->next;
-    free(temp);
+	if (*stk_tete == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", useless_ctr);
+		fclose(monty_bus.monty_file);
+		free(monty_bus.line_content);
+		free_stack(*stk_tete);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stk_tete;
+	*stk_tete = temp->next;
+	free(temp);
 }
