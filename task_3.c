b0c8fe21d52655d626d251941b73056ce4b255f6
Kirito-YO <ussf.lord@gmail>
2023-09-21 15:39:25 -0700

    fix betty

--- task_3.c
+++ task_3.c
@@ -7,25 +7,25 @@
  */
 void swap_objects(stack_t **stk_tete, unsigned int useless_ctr)
 {
-    stack_t *temp;
-    int len = 0, aux;
-
-    temp = *stk_tete;
-    while (temp)
-    {
-        temp = temp->next;
-        len++;
-    }
-    if (len < 2)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", useless_ctr);
-        fclose(monty_bus.monty_file);
-        free(monty_bus.line_content);
-        free_stack(*stk_tete);
-        exit(EXIT_FAILURE);
-    }
-    temp = *stk_tete;
-    aux = temp->n;
-    temp->n = temp->next->n;
-    temp->next->n = aux;
+	stack_t *temp;
+	int len = 0, aux;
+	
+	temp = *stk_tete;
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", useless_ctr);
+		fclose(monty_bus.monty_file);
+		free(monty_bus.line_content);
+		free_stack(*stk_tete);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stk_tete;
+	aux = temp->n;
+	temp->n = temp->next->n;
+	temp->next->n = aux;
 }
