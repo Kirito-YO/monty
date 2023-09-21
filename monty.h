6fbe5a649055e410cc3c1960aaeeaed9e79472bf
Kirito-YO <ussf.lord@gmail>
2023-09-21 15:00:53 -0700

    fix betty

--- monty.h
+++ monty.h
@@ -1,3 +1,43 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 /**
  * struct MontyBus - Variables for Monty interpreter
  * @arg_value: Argument value
@@ -8,9 +48,15 @@
  */
 typedef struct MontyBus
 {
-    char *arg_value;
-    FILE *monty_file;
-    char *line_content;
-    int stack_mode;
+	char *arg_value;
+	FILE *monty_file;
+	char *line_content;
+	int stack_mode;
 } MontyBus;
 extern MontyBus monty_bus;
+
+void print_stack(stack_t **stack_head, unsigned int unused_counter);
+void push_to_stack(stack_t **stack_head, unsigned int line_number);
+void print_top(stack_t **stack_head, unsigned int line_number);
+
+#endif
