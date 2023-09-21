b7a87ece5277c5a99596dcac1f506a04a8b321a9
aouraghse <aouraghse@gmail.com>
2023-09-21 15:44:10 -0700

    task 5

---
+++ task_5.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * f_nothing - Does nothing
+ * @stk_tete: Pointer to the head of the stack
+ * @useless_ctr: Line number
+ * Return: No return
+ */
+void f_nothing(stack_t **stk_tete, unsigned int useless_ctr)
+{
+    (void)useless_ctr;
+    (void)stk_tete;
+}
