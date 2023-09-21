a53f0adaa6f97b265d732cde5d7fe628a6ae2562
Kirito-YO <ussf.lord@gmail>
2023-09-21 14:46:55 -0700

    fix betty

--- task_1.c
+++ task_1.c
@@ -7,13 +7,13 @@
  */
 void print_top(stack_t **stack_head, unsigned int line_number)
 {
-    if (*stack_head == NULL)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        fclose(monty_bus.monty_file);
-        free(monty_bus.line_content);
-        free_stack(*stack_head);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack_head)->n);
+	if (*stack_head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fclose(monty_bus.monty_file);
+		free(monty_bus.line_content);
+		free_stack(*stack_head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack_head)->n);
 }
